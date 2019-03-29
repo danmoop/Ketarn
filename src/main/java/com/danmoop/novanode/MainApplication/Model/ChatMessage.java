a9dda8503d30be9ada4f02b916a809c5bfdbaf13
dan322 <dandurnev0@gmail.com>
2019-03-29 14:05:07 +0300

    new images

--- src/main/java/com/danmoop/novanode/MainApplication/Model/ChatMessage.java
+++ src/main/java/com/danmoop/novanode/MainApplication/Model/ChatMessage.java
@@ -2,25 +2,8 @@ package com.danmoop.novanode.MainApplication.Model;
 
 public class ChatMessage
 {
-    private MessageType type;
     private String text;
     private String sender;
-
-    public enum MessageType {
-        CHAT,
-        JOIN,
-        LEAVE
-    }
-
-    public MessageType getType()
-    {
-        return type;
-    }
-
-    public void setType(MessageType type)
-    {
-        this.type = type;
-    }
 
     public String getText()
     {
